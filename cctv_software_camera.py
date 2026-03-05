9ac81efb65a334ba782ba5fb5735484d886dc74a
Kimberly Alvarado <kimmyjugado@gmail.com>
2026-03-05 10:47:08 +0800

    I still continue the code

--- cctv_software_camera.py
+++ cctv_software_camera.py
@@ -14,35 +14,35 @@ def cctv():
     height = video.get(4)
     print ("Video resolution is set to ", width, 'x', height)
     print ("Help -- \n1.Press esc key to exit. \n2.press m to minimize.")
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     date_time = time.strftime("recording %H-%M-%d -%m %y")
-    output = cv2.VideoWriter('footage/'+date_time+'.mp4', fourcc, 20.0, (width, height))
+    output = cv2.VideoWriter('footage/'+date_time+'.mp4', fourcc, 20.0, (640, 480))
     while video.isOpened():
         check, frame = video.read()
-        if check == True:
+        if True:
             frame = cv2.flip(frame, 1)
 
             t_time = time.ctime()
             cv2.rectangle(frame, (5,5,100,20), (255, 255, 255), cv2.FILLED)
             cv2.putText (frame, "Camera 1", (20,20), cv2.FONT_HERSHEY_DUPLEX, 0.5, (5, 5, 5), 1)
+            cv2.putText(frame, t_time, (420, 460), cv2.FONT_HERSHEY_DUPLEX, 0.5, (5, 5, 5), 1)
             cv2.imshow('CCTV camera', frame)
+
             output.write(frame)
             if cv2.waitKey(1) == 27:
                 print ("Video footage saved in current directory")
                 break
             elif cv2.waitKey(1) == ord('m'):
                 minimizeWindow()
-            else:
-                print("cannot open camera, Check configuration")
-                break
+
     video.release()
     output.release()
     cv2.destroyAllWindows()
-print ("*"*80+"\n"+" "*30"Welome to cctv software\n"+"*" *80)
+print ("*"*80+"\n"+" "*30+"Welcome to cctv software\n"+"*" *80)
 ask = int(input("Do you want to open the cctv?\n1. yes\n2. no\n>>> "))
 
 if ask == 1:
     cctv()
 elif ask == 2:
-    print ("babye! be safe")
-    exit
+    print ("bye bye! be safe")
+    exit()
